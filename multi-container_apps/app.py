a8b53d8ef862f7a4bb4c2b566b96a4319be05954
kemo <kemo3855@yahoo.com>
2024-05-10 16:57:09 +0800

    run postgresql on docker

--- multi-container_apps/app.py
+++ multi-container_apps/app.py
@@ -4,16 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Docker!'
+    return 'Hello, Docker multi container apps!'
 
 if __name__ == '__main__':
     port = 5000
     app.run(host='0.0.0.0', port=port)
     print(f"Server is running on http://0.0.0.0:{port}")
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
